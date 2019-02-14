164bbbdf4ace0dd58197bf4a18b7a702a9174d36
abhishekbvs <bvsabhishek@gmail.com>
2019-02-14 20:50:40 +0530

    Home and Registration apps creation

--- carator/settings.py
+++ carator/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -113,7 +115,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL ='/accounts/login'
 
+LOGIN_REDIRECT_URL = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
---
+++ registration/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from registration.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
