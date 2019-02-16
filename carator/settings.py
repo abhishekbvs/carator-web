f853f59d20669c4cdf47f6661f529a2910d4342f
abhishekbvs <bvsabhishek@gmail.com>
2019-02-16 16:33:30 +0530

    Removed Unnecessary files

--- carator/settings.py
+++ carator/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'registration'
+    'registration',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -104,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LANGUAGE_CODE = 'en-us'
 
